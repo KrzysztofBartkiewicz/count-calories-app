6b638adbf34fd58ea860a16cb3315b8d3587f8bf
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2020-08-01 10:03:48 +0200

    Updates

---
+++ precache-manifest.885a7a4b2f6a9770f1373c29c4277e48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45c493821203c8cad71a151148db5a6",
+    "url": "/count-calories-app/index.html"
+  },
+  {
+    "revision": "150b4f2e805c4ab911c2",
+    "url": "/count-calories-app/static/css/main.4089399a.chunk.css"
+  },
+  {
+    "revision": "565e45642d73ed292980",
+    "url": "/count-calories-app/static/js/2.f356dff8.chunk.js"
+  },
+  {
+    "revision": "9545d2ff040f1d230f20c359158fcfad",
+    "url": "/count-calories-app/static/js/2.f356dff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150b4f2e805c4ab911c2",
+    "url": "/count-calories-app/static/js/main.11c2680f.chunk.js"
+  },
+  {
+    "revision": "d283bc289f7202102d6a",
+    "url": "/count-calories-app/static/js/runtime-main.3e1bd4fa.js"
+  },
+  {
+    "revision": "ae08c1abb6b1f2adb3d06efabb00a9d5",
+    "url": "/count-calories-app/static/media/icon-search.ae08c1ab.svg"
+  }
+]);
