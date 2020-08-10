8541163193d63f6e7b6a26ec18e5071eb0083ebb
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2020-08-10 14:16:02 +0200

    feat: add custom heading component

---
+++ src/components/custom-heading/Heading.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './heading.scss';
+
+const Heading = ({ children }) => <h2 className="custom-heading">{children}</h2>;
+
+export default Heading;
--- src/pages/personal-data/PersonalData.jsx
+++ src/pages/personal-data/PersonalData.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CustomCheckbox from '../../components/custom-checkbox/CustomCheckbox';
+import Heading from '../../components/custom-heading/Heading';
 
 import './personalData.scss';
 
@@ -8,7 +9,7 @@ class PersonalData extends Component {
     return (
       <form className="personal-data">
         <section className="gender">
-          <h2 className="gender__heading">Płeć</h2>
+          <Heading>Płeć</Heading>
           <div className="gender__selection">
             <CustomCheckbox id='female' type='radio' name='gender'>
               {'Kobieta'}
