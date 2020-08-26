3d22b5232295fc1293b238fb578b9f501b619948
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2020-08-26 17:25:34 +0200

    refactor: make data section components dinamicly rendered

--- src/pages/personal-data/PersonalData.jsx
+++ src/pages/personal-data/PersonalData.jsx
@@ -18,38 +18,81 @@ const reg = {
 
 class PersonalData extends Component {
   state = {
-    gender: 'female',
-    age: 38,
-    weight: 90,
-    height: 180,
-    activity: 'low',
-    ageWarn: '',
-    weightWarn: '',
-    heightWarn: '',
+    numberInputs: [
+      {
+        heading: 'Wiek',
+        value: 38,
+        name: 'age',
+        isIncorrect: false,
+        warningMessage: '14 - 100 lat',
+      },
+      {
+        heading: 'Waga',
+        value: 90,
+        name: 'weight',
+        isIncorrect: false,
+        warningMessage: '40 - 200 kg',
+      },
+      {
+        heading: 'Wzrost',
+        value: 180,
+        name: 'height',
+        isIncorrect: false,
+        warningMessage: '120 - 220 cm',
+      },
+    ],
+    genderCheckboxes: [
+      {
+        id: 'female',
+        title: 'Kobieta',
+        isChecked: true,
+      },
+      {
+        id: 'male',
+        title: 'Mężczyzna',
+        isChecked: false,
+      },
+    ],
+    activityCheckboxes: [
+      {
+        id: 'low',
+        title: 'Niska',
+        isChecked: true,
+      },
+      {
+        id: 'medium',
+        title: 'Średnia',
+        isChecked: false,
+      },
+      {
+        id: 'high',
+        title: 'Wysoka',
+        isChecked: false,
+      },
+    ],
+
     showModal: false,
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
 
-    const {
-      gender,
-      activity,
-      ageWarn,
-      weightWarn,
-      heightWarn,
-      age,
-      weight,
-      height,
-    } = this.state;
+    const disableSubmit = this.state.numberInputs.some(
+      (input) => input.isIncorrect === true
+    );
+    if (disableSubmit) return;
 
-    if (ageWarn || weightWarn || heightWarn) return;
+    this.state.numberInputs.forEach((input) => {
+      localStorage.setItem(input.name, input.value);
+    });
 
-    localStorage.setItem('gender', gender);
-    localStorage.setItem('age', age);
-    localStorage.setItem('weight', weight);
-    localStorage.setItem('height', height);
-    localStorage.setItem('activity', activity);
+    this.state.genderCheckboxes.forEach(checkbox => {
+      if (checkbox.isChecked) localStorage.setItem('gender', checkbox.id);
+    })
+
+    this.state.activityCheckboxes.forEach(checkbox => {
+      if (checkbox.isChecked) localStorage.setItem('activity', checkbox.id);
+    })
 
     this.setState({ showModal: true });
 
@@ -59,49 +102,34 @@ class PersonalData extends Component {
     }, 3000);
   };
 
-  handleChange = (event) => {
+  handleChange = (event, index) => {
     const { name, value } = event.target;
 
-    switch (name) {
-      case 'age':
-        reg.age.test(value)
-          ? this.setState({ ageWarn: '' })
-          : this.setState({ ageWarn: '14 - 100 lat' });
-        break;
-      case 'weight':
-        reg.weight.test(value)
-          ? this.setState({ weightWarn: '' })
-          : this.setState({ weightWarn: '40 - 200 kg' });
-        break;
-      case 'height':
-        reg.height.test(value)
-          ? this.setState({ heightWarn: '' })
-          : this.setState({ heightWarn: '120 - 220 cm' });
-        break;
-      default:
-        break;
-    }
+    this.setState((prevState) => {
+      prevState.numberInputs[index].value = value;
 
-    this.setState({ [name]: value });
+      if (reg[name].test(value)) {
+        prevState.numberInputs[index].isIncorrect = false;
+      } else {
+        prevState.numberInputs[index].isIncorrect = true;
+      }
+
+      return prevState;
+    });
   };
 
-  handleCheck = (event) => {
-    const { name, id } = event.target;
-    this.setState({ [name]: id });
+  handleCheck = (event, index) => {
+    const { name } = event.target;
+
+    this.setState((prevState) => {
+      prevState[name].forEach((checkbox) => (checkbox.isChecked = false));
+      prevState[name][index].isChecked = true;
+      return prevState;
+    });
   };
 
   render() {
-    const {
-      gender,
-      activity,
-      ageWarn,
-      weightWarn,
-      heightWarn,
-      showModal,
-      age,
-      weight,
-      height,
-    } = this.state;
+    const { showModal } = this.state;
 
     return (
       <Fragment>
@@ -121,88 +149,51 @@ class PersonalData extends Component {
           autoComplete="off"
         >
           <DataSection heading={<Heading>{'Płeć'}</Heading>}>
-            <CustomCheckbox
-              checked={gender === 'female'}
-              onChange={this.handleCheck}
-              id="female"
-              type="radio"
-              name="gender"
+            {this.state.genderCheckboxes.map((checkbox, index) => (
+              <CustomCheckbox
+                key={checkbox.id}
+                checked={checkbox.isChecked}
+                onChange={(event) => this.handleCheck(event, index)}
+                id={checkbox.id}
+                type="radio"
+                name="genderCheckboxes"
+              >
+                {checkbox.title}
+              </CustomCheckbox>
+            ))}
+          </DataSection>
+
+          {this.state.numberInputs.map((input, index) => (
+            <DataSection
+              key={input.name}
+              heading={<Heading>{input.heading}</Heading>}
             >
-              {'Kobieta'}
-            </CustomCheckbox>
-            <CustomCheckbox
-              checked={gender === 'male'}
-              onChange={this.handleCheck}
-              id="male"
-              type="radio"
-              name="gender"
-            >
-              {'Mężczyzna'}
-            </CustomCheckbox>
-          </DataSection>
-
-          <DataSection heading={<Heading>{'Wiek'}</Heading>}>
-            <CustomTextInput
-              value={age}
-              onChange={this.handleChange}
-              type="number"
-              name="age"
-              warning={ageWarn}
-            />
-          </DataSection>
-
-          <DataSection heading={<Heading>{'Waga (kg)'}</Heading>}>
-            <CustomTextInput
-              value={weight}
-              onChange={this.handleChange}
-              type="number"
-              name="weight"
-              warning={weightWarn}
-            />
-          </DataSection>
-
-          <DataSection heading={<Heading>{'Wzrost (cm)'}</Heading>}>
-            <CustomTextInput
-              value={height}
-              onChange={this.handleChange}
-              type="number"
-              name="height"
-              warning={heightWarn}
-            />
-          </DataSection>
+              <CustomTextInput
+                value={input.value}
+                onChange={(event) => this.handleChange(event, index)}
+                type="number"
+                name={input.name}
+                warning={input.isIncorrect ? input.warningMessage : null}
+              />
+            </DataSection>
+          ))}
 
           <DataSection heading={<Heading>{'Aktywność'}</Heading>}>
-            <CustomCheckbox
-              checked={activity === 'low'}
-              onChange={this.handleCheck}
-              id="low"
-              type="radio"
-              name="activity"
-              orientation="bottom"
-            >
-              {'Niska'}
-            </CustomCheckbox>
-            <CustomCheckbox
-              checked={activity === 'medium'}
-              onChange={this.handleCheck}
-              id="medium"
-              type="radio"
-              name="activity"
-              orientation="bottom"
-            >
-              {'Średnia'}
-            </CustomCheckbox>
-            <CustomCheckbox
-              checked={activity === 'high'}
-              onChange={this.handleCheck}
-              id="high"
-              type="radio"
-              name="activity"
-              orientation="bottom"
-            >
-              {'Wysoka'}
-            </CustomCheckbox>
+            {this.state.activityCheckboxes.map((checkbox, index) => (
+              <CustomCheckbox
+                key={checkbox.id}
+                checked={checkbox.isChecked}
+                onChange={(event) => this.handleCheck(event, index)}
+                id={checkbox.id}
+                type="radio"
+                name="activityCheckboxes"
+                orientation="bottom"
+              >
+                {checkbox.title}
+              </CustomCheckbox>
+            ))}
           </DataSection>
+
           <Button type="submit" btnType="save">
             {'Zapisz'}
           </Button>
